ad02255f03f1ccff8a744b1c033682c50a577032
John Lester Escarlan <jlescarlan11@gmail.com>
2026-07-28 04:54:20 +0800

    refactor(home): break self barrel dependency

--- src/features/home/index.ts
+++ src/features/home/index.ts
@@ -1,5 +1,2 @@
-export { heroContent } from './content';
 export { default as HomePage } from './HomePage';
-export { default as HeroSection } from './components/HeroSection';
-export { default as FooterSection } from './components/FooterSection';
 export { default as ProfileStructuredData } from './components/ProfileStructuredData';
